94cd7d3e892076d502b7951ddab191084c686ca7
Abhinav-Kotta <abhinav.kotta@gmail.com>
2023-10-14 01:34:22 -0400

    colorful stuff

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -6,6 +6,7 @@ import { signInWithRedirect, signOut } from "firebase/auth";
 import { auth, githubProvider } from "./firebase";
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Particles from 'react-tsparticles';
 
 function App() {
   return (
@@ -16,12 +17,18 @@ function App() {
       minHeight="100vh" 
       flexDirection="column"
       gap={2}
+      sx={{
+        background: 'linear-gradient(45deg, #09203F 30%, #537895 90%)',
+        position: 'relative',
+        overflow: 'hidden'
+      }}
     >
+    
       <div>
-        <Button variant="outlined" onClick={() => signInWithRedirect(auth, githubProvider)}>
-          Sign in
-        </Button>
-        <Button variant="outlined" onClick={() => signOut(auth)}>Sign out</Button>
+        <Box display="flex" flexDirection="row" gap={2}>
+          <Button variant="outlined" onClick={() => signInWithRedirect(auth, githubProvider)}>Sign in</Button>
+          <Button variant="outlined" onClick={() => signOut(auth)}>Sign out</Button>
+        </Box>
       </div>
     </Box>
   );
