b87e09b1773f6c4d0609a94b0ffcf5655359537f
Shaunak Warty <shaunak.warty@gmail.com>
2023-10-14 14:33:31 -0400

    bug fix

--- website/src/components/UserHome.tsx
+++ website/src/components/UserHome.tsx
@@ -14,9 +14,10 @@ interface IProps {
 
 function UserHome(props: IProps) {
   const [value, setValue] = useState(0);
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
+  console.log(props);
   return (
     <>
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
